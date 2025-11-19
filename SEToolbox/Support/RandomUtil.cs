27b2f723cc308fc38b215a1a23500e8ac53259ab
Omiyaru <c.durrenberger@gmail.com>
2025-11-19 18:29:48 -0500

    Update RandomUtil.cs
    fixed error with seed generation

--- SEToolbox/Support/RandomUtil.cs
+++ SEToolbox/Support/RandomUtil.cs
@@ -8,17 +8,16 @@ namespace SEToolbox.Support
     {
         static readonly Guid _guid = Guid.NewGuid();
 
-        static readonly int _seed = 0;
+        static int _seed;
         static readonly int _secretSeed = BitConverter.ToInt32(BitConverter.GetBytes(DateTime.Now.Ticks), 0) ^ BitConverter.ToInt32(Encoding.UTF8.GetBytes(_guid.ToString()), 0);
 
-        static readonly ThreadLocal<Random> _threadLocalRandom = new(() => new Random(_seed)null);
+        static readonly ThreadLocal<Random> _threadLocalRandom = new(() => new Random(_seed));
 
         public static Random MyRandom
         {
 
             get => _threadLocalRandom.Value;
             set => _threadLocalRandom.Value = value;
-           
         }
 
         public static bool EnableSecretRandom
@@ -26,23 +25,21 @@ namespace SEToolbox.Support
             get => _threadLocalRandom.IsValueCreated;
             set => _threadLocalRandom.Value = value ? new Random(_secretSeed)  : null; 
         }
-
+      
         public static void SetSeed(int? seed = null,int? value = null, bool isSecretRandom = false)
         {
             if (seed.HasValue && seed.Value != 0)
             {
-                bool enableSecretRandom = isSecretRandom;
                 if (_threadLocalRandom.IsValueCreated)
                     _threadLocalRandom.Value = new Random(seed.Value);
                 if (!seed.HasValue)
-                    _threadLocalRandom.Value = new Random(_secretSeed);
+                    _threadLocalRandom.Value = new Random(_seed);
                 if (isSecretRandom)
-
                    EnableSecretRandom = true;
             }
-            if (value.HasValue ?? false)
+            if (seed.HasValue)
             {
-                _seed = value.Value;
+               _seed = seed.Value;
             }
         }
   
