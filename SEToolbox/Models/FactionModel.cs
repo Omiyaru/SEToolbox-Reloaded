170a65f3df2db8d86f907b153f01d2198d542d9c
Omiyaru <c.durrenberger@gmail.com>
2025-11-20 10:39:26 -0500

    Refactor FactionModel property definitions
    Converted auto-properties to explicit property blocks for improved readability and maintainability. Also fixed inconsistent indentation and formatting throughout the file.

--- SEToolbox/Models/FactionModel.cs
+++ SEToolbox/Models/FactionModel.cs
@@ -69,7 +69,7 @@ namespace SEToolbox.Models
         private List<MyFactionIconsDefinition> _icons;
 
         private ConcurrentDictionary<MyPlayer, List<Tuple<int, float, MyPlayer>>> _playerToPlayerRelationship;
-        private ConcurrentDictionary<MyPlayer, List<Tuple<int, float, MyFaction>>>  _factionToPlayerRelationship;
+        private ConcurrentDictionary<MyPlayer, List<Tuple<int, float, MyFaction>>> _factionToPlayerRelationship;
         private ConcurrentDictionary<MyFaction, List<Tuple<int, float, MyFaction>>> _factionToFactionRelationship;
 
         private int _reputation;
@@ -83,58 +83,225 @@ namespace SEToolbox.Models
         #region Properties
 
 
-        public List<MyFactionIconsDefinition> Icons { get => _icons; set => SetProperty(ref _icons, value); }
-        public Dictionary<long, MyFaction> Factions { get => _factions; set => SetProperty(ref _factions, value); }
-        public Dictionary<long, MyFactionMember> Members { get => _members; set => SetProperty(ref _members, value); }
-        public Dictionary<long, MyPlayer> SelectedPlayers { get => _selectedPlayers; set => SetProperty(ref _selectedPlayers, value); }
-        public Dictionary<long, MyFactionMember> SelectedMembers { get => _selectedMembers; set => SetProperty(ref _selectedMembers, value); }
+        public List<MyFactionIconsDefinition> Icons  
+        { 
+            get => _icons; 
+            set => SetProperty(ref _icons, value); }
+        public Dictionary<long, MyFaction> Factions  
+        { 
+            get => _factions; 
+            set => SetProperty(ref _factions, value); }
+        public Dictionary<long, MyFactionMember> Members  
+        { 
+            get => _members; 
+            set => SetProperty(ref _members, value); }
+        public Dictionary<long, MyPlayer> SelectedPlayers  
+        { 
+            get => _selectedPlayers; 
+            set => SetProperty(ref _selectedPlayers, value); }
+        public Dictionary<long, MyFactionMember> SelectedMembers  
+        { 
+            get => _selectedMembers; 
+            set => SetProperty(ref _selectedMembers, value); }
         public ConcurrentDictionary<MyFaction, List<Tuple<int, float, MyFaction>>> FactionToFactionRelationship;
         public ConcurrentDictionary<MyFaction, List<Tuple<int, float, MyPlayer>>> FactionToPlayerRelationship;
         public ConcurrentDictionary<MyPlayer, List<Tuple<int, float, MyPlayer>>> PlayerToPlayerRelationship;
         private WorkshopId? _factionIconWorkshopId;
         private Dictionary<long, MyFactionMember> _selectedMembers;
 
-        public string Name { get => _factionName; set => SetProperty(ref _factionName, value, nameof(Name)); }
-        public string Tag { get => _tag; set => SetProperty(ref _tag, value, nameof(Tag)); }
+        public string Name  
+        { 
+            get => _factionName; 
+            set => SetProperty(ref _factionName, nameof(Name)); 
+        }
+        public string Tag  
+        { 
+            get => _tag; 
+            set => SetProperty(ref _tag, nameof(Tag)); 
+        }
 
-        public string FactionTypeString { get => _factionTypeString; set => SetProperty(ref _factionTypeString, value, nameof(FactionTypeString)); }
-        public string Description { get => _factionDescription; set => SetProperty(ref _factionDescription, value, nameof(Description)); }
-        public string PrivateInfo { get => _privateInfo; set => SetProperty(ref _privateInfo, value, nameof(PrivateInfo)); }
-        public WorkshopId? FactionIconWorkshopId { get => _factionIconWorkshopId; set => SetProperty(ref _factionIconWorkshopId, value, nameof(FactionIconWorkshopId)); }
-        public string FactionLeader { get => _factionLeader; set => SetProperty(ref _factionLeader, value, nameof(FactionLeader)); }
-        public MyStringId? FactionIcon { get => _factionIcon; set => SetProperty(ref _factionIcon, value, nameof(FactionIcon)); }
-        public int Score { get => _score; set => SetProperty(ref _score, value, nameof(Score)); }
-        public string FactionType { get => _factionType; set => SetProperty(ref _factionType, value, nameof(FactionType)); }
-        public long FactionId { get => _factionId; set => SetProperty(ref _factionId, value); }
-        public int Balance { get => _balance; set => SetProperty(ref _balance, value, nameof(Balance)); }
-        public MyStringId? FactionIconString { get => _factionIconString; set => SetProperty(ref _factionIconString, value, nameof(FactionIconString)); }
-        public string FactionPrivateInfo { get => _factionPrivateInfo; set => SetProperty(ref _factionPrivateInfo, value, nameof(FactionPrivateInfo)); }
-        public SVector3 FactionColor { get => _factionColor; set => SetProperty(ref _factionColor, value, nameof(FactionColor)); }
-        public Vector3 IconColor { get => _iconColor; set => SetProperty(ref _iconColor, value, nameof(IconColor)); }
-        public Vector3 CustomColor { get => _customColor; set => SetProperty(ref _customColor, value, nameof(CustomColor)); }
-        public long FounderId { get => _founderId; set => SetProperty(ref _founderId, value, nameof(FounderId)); }
-        public int MemberCount { get => _memberCount; set => SetProperty(ref _memberCount, value, nameof(MemberCount)); }
-        public float ObjectivePercentageCompleted { get => _objectivePercentageCompleted; set => SetProperty(ref _objectivePercentageCompleted, value, nameof(ObjectivePercentageCompleted)); }
+        public string FactionTypeString  
+        { 
+            get => _factionTypeString; 
+            set => SetProperty(ref _factionTypeString, nameof(FactionTypeString)); 
+        }
+        public string Description  
+        { 
+            get => _factionDescription; 
+            set => SetProperty(ref _factionDescription, nameof(Description)); 
+        }
+        public string PrivateInfo  
+        { 
+            get => _privateInfo; 
+            set => SetProperty(ref _privateInfo, nameof(PrivateInfo)); 
+        }
+        public WorkshopId? FactionIconWorkshopId  
+        { 
+            get => _factionIconWorkshopId; 
+            set => SetProperty(ref _factionIconWorkshopId, nameof(FactionIconWorkshopId)); 
+        }
+        public string FactionLeader  
+        { 
+            get => _factionLeader; 
+            set => SetProperty(ref _factionLeader, nameof(FactionLeader)); 
+        }
+        public MyStringId? FactionIcon  
+        { 
+            get => _factionIcon; 
+            set => SetProperty(ref _factionIcon, nameof(FactionIcon)); 
+        }
+        public int Score  
+        { 
+            get => _score; 
+            set => SetProperty(ref _score, nameof(Score)); 
+        }
+        public string FactionType  
+        { 
+            get => _factionType; 
+            set => SetProperty(ref _factionType, nameof(FactionType)); 
+        }
+        public long FactionId  
+        { 
+            get => _factionId; 
+            set => SetProperty(ref _factionId, nameof(FactionId)); 
+        }
+        public int Balance  
+        { 
+            get => _balance; 
+            set => SetProperty(ref _balance, nameof(Balance)); 
+        }
+        public MyStringId? FactionIconString  
+        { 
+            get => _factionIconString; 
+            set => SetProperty(ref _factionIconString, nameof(FactionIconString)); 
+        }
+        public string FactionPrivateInfo  
+        { 
+            get => _factionPrivateInfo; 
+            set => SetProperty(ref _factionPrivateInfo, nameof(FactionPrivateInfo)); 
+        }
+        public SVector3 FactionColor  
+        { 
+            get => _factionColor; 
+            set => SetProperty(ref _factionColor, nameof(FactionColor)); 
+        }
+        public Vector3 IconColor  
+        { 
+            get => _iconColor; 
+            set => SetProperty(ref _iconColor, nameof(IconColor)); 
+        }
+        public Vector3 CustomColor  
+        { 
+            get => _customColor; 
+            set => SetProperty(ref _customColor, nameof(CustomColor)); 
+        }
+        public long FounderId  
+        { 
+            get => _founderId; 
+            set => SetProperty(ref _founderId, nameof(FounderId)); 
+        }
+        public int MemberCount  
+        { 
+            get => _memberCount; 
+            set => SetProperty(ref _memberCount, nameof(MemberCount)); 
+        }
+        public float ObjectivePercentageCompleted  
+        { 
+            get => _objectivePercentageCompleted; 
+            set => SetProperty(ref _objectivePercentageCompleted, nameof(ObjectivePercentageCompleted)); 
+        }
 
-        public long Member { get => _member; set { _member = value; SetProperty(ref _member, value, nameof(Member)); } }
-        public int FactionIconId { get => _factionIconId; set { _factionIconId = value; SetProperty(ref _factionIconId, value, nameof(FactionIconId)); } }
-         public int FactionCount { get => _factionCount; set { _factionCount = value; SetProperty(ref _factionCount, value, nameof(FactionCount)); } }
-        public bool IsNpcFaction { get => _isNpcFaction; set => SetProperty(ref _isNpcFaction, value); }
-        public MyFaction SelectedFaction { get => _selectedFaction; set => SetProperty(ref _selectedFaction, value, nameof(SelectedFaction)); }
-        public MyFactionMember SelectedMember { get => _selectedMember; set => SetProperty(ref _selectedMember, value, nameof(SelectedMember)); }
-        public MyFaction Faction { get => _faction; set => SetProperty(ref _faction, value, nameof(Faction)); }
-        public IFactionBase SelectedPlayer { get => _selectedPlayer; set => SetProperty(ref _selectedPlayer, value, nameof(SelectedPlayer)); }
-        public int Reputation { get => _reputation; set => SetProperty(ref _reputation, value, nameof(Reputation)); }
-        public int FactionReputation { get => _factionReputation; set => SetProperty(ref _factionReputation, value, nameof(FactionReputation)); } // holder for when the factions reputation Reputation = FactionReputation
-        public int PlayerReputation { get => _playerReputation; set => SetProperty(ref _playerReputation, value, nameof(PlayerReputation)); } //this is a holder for the players reputation   Reputation = PlayerReputation
-         public long StationId { get => _stationId; set => SetProperty(ref _stationId, value, nameof(StationId)); }
-        public string StationName { get => _stationName; set => SetProperty(ref _stationName, value, nameof(StationName)); }
-        public MyStationTypeEnum StationType { get => _stationType; set => SetProperty(ref _stationType, value, nameof(StationType)); }
-        public ObservableCollection<MyPlayer> Players { get => _players; set => SetProperty(ref _players, value, nameof(Players)); }
-        public bool? IsFactionSelected { get => _isFactionSelected; set => SetProperty(ref _isFactionSelected, value, nameof(IsFactionSelected)); }
+        public long Member  
+        { 
+            get => _member; 
+            set => SetProperty(ref _member, nameof(Member)); 
+        } 
+        public int FactionIconId  
+        { 
+            get => _factionIconId; 
+            set => SetProperty(ref _factionIconId, nameof(FactionIconId)); 
+        } 
+         public int FactionCount 
+         { 
+            get => _factionCount; 
+            set => SetProperty(ref _factionCount, nameof(FactionCount)); 
+        } 
+        public bool IsNpcFaction  
+        { 
+            get => _isNpcFaction; 
+            set => SetProperty(ref _isNpcFaction, nameof(IsNpcFaction)); 
+        }
+        public MyFaction SelectedFaction  
+        { 
+            get => _selectedFaction; 
+            set => SetProperty(ref _selectedFaction, nameof(SelectedFaction)); 
+        }
+        public MyFactionMember SelectedMember  
+        { 
+            get => _selectedMember; 
+            set => SetProperty(ref _selectedMember, nameof(SelectedMember)); 
+        }
+        public MyFaction Faction  
+        { 
+            get => _faction; 
+            set => SetProperty(ref _faction, nameof(Faction)); 
+        }
+        public IFactionBase SelectedPlayer  
+        { 
+            get => _selectedPlayer; 
+            set => SetProperty(ref _selectedPlayer, nameof(SelectedPlayer)); 
+        }
+        public int Reputation  
+        { 
+            get => _reputation; 
+            set => SetProperty(ref _reputation, nameof(Reputation)); 
+        }
+        public int FactionReputation  
+        { 
+            get => _factionReputation; 
+            set => SetProperty(ref _factionReputation, nameof(FactionReputation)); 
+        } // holder for when the factions reputation Reputation = FactionReputation
+        public int PlayerReputation  
+        { 
+            get => _playerReputation; 
+            set => SetProperty(ref _playerReputation, nameof(PlayerReputation)); 
+        } //this is a holder for the players reputation   Reputation = PlayerReputation
+         public long StationId  
+        { 
+            get => _stationId; 
+            set => SetProperty(ref _stationId, nameof(StationId)); 
+        }
+        public string StationName  
+        { 
+            get => _stationName; 
+            set => SetProperty(ref _stationName, nameof(StationName)); 
+        }
+        public MyStationTypeEnum StationType  
+        { 
+            get => _stationType; 
+            set => SetProperty(ref _stationType, nameof(StationType)); 
+        }
+        public ObservableCollection<MyPlayer> Players  
+        { 
+            get => _players; 
+            set => SetProperty(ref _players, nameof(Players)); 
+        }
+        public bool? IsFactionSelected  
+        { 
+            get => _isFactionSelected; 
+            set => SetProperty(ref _isFactionSelected, nameof(IsFactionSelected)); 
+        }
 
-        public string Leader { get => _leader; set => SetProperty(ref _leader, value, nameof(Leader)); }
-        public string SelectedFactionType { get => _selectedFactionType; set => SetProperty(ref _selectedFactionType, value, nameof(SelectedFactionType)); }
+        public string Leader  
+        { 
+            get => _leader; 
+            set => SetProperty(ref _leader, nameof(Leader)); 
+        }
+        public string SelectedFactionType  
+        { 
+            get => _selectedFactionType; 
+            set => SetProperty(ref _selectedFactionType, nameof(SelectedFactionType)); 
+        }
 
         #endregion
 
@@ -149,11 +316,11 @@ namespace SEToolbox.Models
             _factionIcon = selectedFaction.FactionIcon;
             _factionId = selectedFaction.FactionId;
             _factionIconString = selectedFaction.FactionIcon;
-             _tag = selectedFaction.Tag;
-             _factionName = selectedFaction.Name;
+            _tag = selectedFaction.Tag;
+            _factionName = selectedFaction.Name;
             _factionLeader = selectedFaction.Members.FirstOrDefault(m => m.Value.IsLeader).Value.PlayerId.ToString() ?? "No Leader";
-              _factionDescription = selectedFaction.Description;
-           _factionPrivateInfo = selectedFaction.PrivateInfo;
+            _factionDescription = selectedFaction.Description;
+            _factionPrivateInfo = selectedFaction.PrivateInfo;
             _memberCount = selectedFaction.Members.Count;
             _founderId = selectedFaction.FounderId;
             _iconColor = selectedFaction.IconColor;
@@ -165,12 +332,12 @@ namespace SEToolbox.Models
             
             
 
-            _factions.Add(selectedFaction.FactionId, selectedFaction);
+           _factions.Add(selectedFaction.FactionId, selectedFaction);
         }
         public FactionModel()
         {
             LoadFactions();
-            _players = [];
+           _players = [];
         }
       
 
@@ -180,7 +347,7 @@ namespace SEToolbox.Models
 
         public void LoadFactions()
         {
-            _factions.Clear();
+           _factions.Clear();
 
             var sessionFactions = SGW.MySession.Static?.Factions;
             if (sessionFactions != null)
@@ -189,7 +356,7 @@ namespace SEToolbox.Models
                 {
                     if (!_factions.ContainsKey(factionPair.Key))
                     {
-                        _factions.Add(factionPair.Key, factionPair.Value);
+                       _factions.Add(factionPair.Key, factionPair.Value);
                     }
                 }
             }
@@ -385,7 +552,7 @@ namespace SEToolbox.Models
                 faction = new MyFaction(factionId, tag, name, description, privateInfo,
                     founderId, factionType, customColor, factionIconColor,
                     factionIcon, factionWorkshopId, score, objectivePercentageCompleted);
-                _factions[factionId] = faction;
+               _factions[factionId] = faction;
             }
             else
             {
